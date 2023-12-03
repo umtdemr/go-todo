2b094c1879c2d7848e72177692210f29b74fc9ea
umtdemr <umitde692@gmail.com>
2023-12-03 20:50:18 +0300

    add handleUpdate method to update todo items

--- main.go
+++ main.go
@@ -3,17 +3,20 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/google/uuid"
 	"github.com/umtdemr/go-todo/respond"
 	"github.com/umtdemr/go-todo/todo"
 	"net/http"
+	"slices"
+	"time"
 )
 
 type Database struct {
-	Todos *[]todo.Todo
+	Todos *[]*todo.Todo
 }
 
 var db = Database{
-	Todos: &[]todo.Todo{todo.NewTodo("hey")},
+	Todos: &[]*todo.Todo{todo.NewTodo("initial todo")},
 }
 
 func handleList(w http.ResponseWriter, r *http.Request) {
@@ -42,12 +45,56 @@ func handleAdd(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	*db.Todos = append(*db.Todos, todo.NewTodo(createTodoType.Title))
+	createdTodo := todo.NewTodo(createTodoType.Title)
+	*db.Todos = append(*db.Todos, createdTodo)
+
+	respond.Respond(w, createdTodo)
+}
+
+func handleUpdate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		respond.WithError(w, "Only POST methods are allowed", http.StatusBadRequest)
+		return
+	}
+
+	var updateData todo.UpdateTodoData
+
+	decoder := json.NewDecoder(r.Body)
+
+	err := decoder.Decode(&updateData)
+
+	if err != nil {
+		respond.WithError(w, fmt.Sprintf("error while parsing: %s", err), http.StatusBadRequest)
+		return
+	}
+
+	id, uuidParseErr := uuid.Parse(updateData.Id)
+
+	if uuidParseErr != nil {
+		respond.WithError(w, "the id you sent is invalid", http.StatusBadRequest)
+		return
+	}
+
+	todoIndex := slices.IndexFunc(*db.Todos, func(c *todo.Todo) bool { return c.Id == id })
+	todoItem := (*db.Todos)[todoIndex]
+
+	if updateData.Title != nil {
+		todoItem.Title = *updateData.Title
+	}
+
+	if updateData.Done != nil {
+		todoItem.Done = *updateData.Done
+	}
+
+	todoItem.UpdatedAt = time.Now()
+
+	respond.Respond(w, todoItem)
 }
 
 func main() {
 	http.HandleFunc("/list", handleList)
 	http.HandleFunc("/create", handleAdd)
+	http.HandleFunc("/update", handleUpdate)
 	err := http.ListenAndServe(":8080", nil)
 
 	if err != nil {
--- todo/todo.go
+++ todo/todo.go
@@ -1,8 +1,12 @@
 package todo
 
-import "time"
+import (
+	"github.com/google/uuid"
+	"time"
+)
 
 type Todo struct {
+	Id        uuid.UUID
 	Title     string    `json:"title"`
 	Done      bool      `json:"done"`
 	CreatedAt time.Time `json:"createdAt"`
@@ -13,8 +17,15 @@ type CreateTodoData struct {
 	Title string `json:"title"`
 }
 
-func NewTodo(title string) Todo {
-	return Todo{
+type UpdateTodoData struct {
+	Id    string  `json:"id"`
+	Title *string `json:"title,omitempty"`
+	Done  *bool   `json:"done,omitempty"`
+}
+
+func NewTodo(title string) *Todo {
+	return &Todo{
+		Id:        uuid.New(),
 		Title:     title,
 		CreatedAt: time.Now(),
 		UpdatedAt: time.Now(),
