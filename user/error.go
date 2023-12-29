2f484527924f77e3023da22f9e75a2ab77707418
umtdemr <umitde692@gmail.com>
2023-12-29 18:50:26 +0300

    add email validations

--- user/error.go
+++ user/error.go
@@ -5,6 +5,9 @@ type errKind int
 const (
 	_ errKind = iota
 	lengthUsername
+	userNameNotValid
+	lengthEmail
+	emailNotValid
 )
 
 type UserError struct {
@@ -18,10 +21,19 @@ func (e UserError) Error() string {
 	switch e.kind {
 	case lengthUsername:
 		return "username length should be between 3 and 30"
+	case userNameNotValid:
+		return "username shouldn't include special characters"
+	case lengthEmail:
+		return "email length should be between 6 and 255"
+	case emailNotValid:
+		return "email is not valid"
 	}
 	return "error in user"
 }
 
 var (
-	ErrorUsernameLength = UserError{kind: lengthUsername, fields: []string{"username"}}
+	ErrorUsernameLength   = UserError{kind: lengthUsername, fields: []string{"username"}}
+	ErrorUserNameNotValid = UserError{kind: userNameNotValid, fields: []string{"username"}}
+	ErrorEmailLength      = UserError{kind: lengthEmail, fields: []string{"email"}}
+	ErrorEmailNotValid    = UserError{kind: emailNotValid, fields: []string{"email"}}
 )
--- user/service.go
+++ user/service.go
@@ -1,5 +1,7 @@
 package user
 
+import "regexp"
+
 type Service struct {
 	repository IRepository
 }
@@ -12,6 +14,22 @@ func (service *Service) CreateUser(data *CreateUserData) error {
 	if len(data.Username) < 3 || len(data.Username) > 20 {
 		return ErrorUsernameLength
 	}
+
+	userNameRegex := regexp.MustCompile(`^[a-zA-Z0-9_]+$`)
+	if !userNameRegex.MatchString(data.Username) {
+		return ErrorUserNameNotValid
+	}
+
+	if len(data.Email) < 6 && len(data.Email) > 255 {
+		return ErrorEmailLength
+	}
+
+	emailRegex := regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
+
+	if !emailRegex.MatchString(data.Email) {
+		return ErrorEmailNotValid
+	}
+
 	return service.repository.CreateUser(data)
 }
 
